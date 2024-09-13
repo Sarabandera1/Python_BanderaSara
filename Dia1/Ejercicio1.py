19c7b4fc90e41139e6bb7c817960148f4484137c
Sarabandera <sarabanderaa@gmail.com>
2024-09-13 16:00:46 -0500

    feat: :sparkles:

---
+++ Dia1/Ejercicio1.py
@@ -0,0 +1,7 @@
+#Solicita al usuario que ingrese un número y verifica si es par o impar.
+numero = int (input("Ingrese el numero que desea saber si es par: "))
+if (numero % 2 == 0): 
+    print ("¡su numero es par!")
+else:
+    print ("¡Su numero es impar!")
+    
