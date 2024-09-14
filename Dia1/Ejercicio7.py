73cc3e4eaed60eb37fdd754abb82f3e7f38fe2a5
Sarabandera1 <sarabanderaa@gmail.com>
2024-09-14 17:05:24 -0500

    feat: :sparkles: Ejercicio 7
    números positivos y negativos

---
+++ Dia1/Ejercicio7.py
@@ -0,0 +1,10 @@
+#Solicita al usuario que ingrese un número y determina si es positivo, negativo o cero.
+num= int(input("ingrese el numero: "))
+if (num<0):
+    print("El numero es negativo")
+elif (num>0):
+    print("El numero es positivo")
+else:
+    print ("El numero es cero")
+    
+
