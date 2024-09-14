40a0ac50cad9d9ce8bde1e012f4b8dd3d27daa5f
Sarabandera <sarabanderaa@gmail.com>
2024-09-13 20:02:46 -0500

    feat: :sparkles: ejercicio 4
    Ejercicio 4 de triangulos

--- Dia1/Ejercicio4.py
+++ Dia1/Ejercicio4.py
@@ -1,1 +1,17 @@
 #Solicita al usuario que ingrese las longitudes de los tres lados de un triángulo. Determina si el triángulo es equilátero, isósceles o escaleno.
+
+lado1= float(input("ingrese la medida del lado 1"))
+lado2= float(input("ingrese la medida del lado 2"))
+lado3= float(input("ingrese la medida del lado 3"))
+
+if lado1 == lado2 == lado3:
+    print ("el triangulo es equilatero")
+elif lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
+    print ("El triangulo es isosceles")
+else: 
+    print ("el triangulo es escaleno")
+
+
+
+
+
